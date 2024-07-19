aec5761ad45ab85256850d9deebe235b18de04c3
369webforall <369webforall@gmail.com>
2024-07-19 13:07:46 +0545

    js

---
+++ 4_Javascript/demo_js/script.js
@@ -0,0 +1,64 @@
+console.log("hellow world!!!");
+// types of data -
+
+//string - "any things in quote"
+// number - 1, 2,
+// boolean - true or false
+
+// null, undefined
+
+// array = [1,2,3,4,5,"dev", true, null, "any", []]
+
+// object = car = {color:"Red", model:2024, company:"Toyota", owner:"Dev"}
+
+// what is variable - to store data
+
+let x = 5;
+
+console.log(x);
+
+let y = 10;
+
+let z = x + y;
+
+console.log(z);
+
+let str = "I am dev, i am your mentor";
+
+console.log(str);
+
+console.log("river" + "town");
+console.log("cat" + "dog");
+console.log("New" + " " + " York");
+console.log("runner's knee" + "!");
+console.log("man" + "bear" + "pig");
+
+console.log(19 % 8); // 3
+
+console.log(4 % 7); // 4
+
+let obj = {};
+let val = 5;
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+function Add(a, b) {
+  return a + b;
+}
+
+console.log(Add(5, 6));
+console.log(Add(10, 6));
+
+let arr2 = [1, 5, 8, 10, 11, 20];
+
+for (let i = 0; i < arr2.length; i++) {
+  console.log(arr2[i] * 2);
+}
+
+let obj2 = { name: "dev", age: 45 };
+
+let ans = "name";
+console.log(obj2[ans]);
+console.log(obj2.age);
